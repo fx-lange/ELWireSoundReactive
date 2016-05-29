cacefaa4bf755ebd692f6d5057e31f2de531a804
Felix Lange <langefx@gmail.com>
2016-05-29 17:51:38 +0200

    new web controller

--- bin/data/web/js/guiGlueMod.js
+++ bin/data/web/js/guiGlueMod.js
@@ -20,27 +20,49 @@ function guiGlue(paramsGUI, optionsGUI){
         //initial creation    
         var gui = new dat.GUI(optionsGUI);
 
+        //handle root
+        var rootKey = Object.keys(paramsGUI)[0];
+        var rootFolder = gui.addFolder(rootKey);
+        if (!optionsGUI.folded)
+            rootFolder.open();
+
+        params[rootKey] = {};
+
         //walk the parameter tree
-        unfurl(paramsGUI, gui, params);
+        unfurl(paramsGUI[rootKey], rootFolder, params[rootKey]);
 
         function unfurl(obj, folder, params){
 
-            for (var key in obj){
+            /*
+                instead of just going through all members of obj in one loop,
+                a second loop (outer) is needed to find the members in the right order,
+                because many json implementation don't keep track of the insertion order.
+                for example jsoncpp used by ofxSyncedParams does not ...
+            */
+            
+            var size = Object.keys(obj["members"]).length;
+            for(var i=0;i<size;++i){
 
-                var subObj = obj[key];
-                var leaf = isLeaf(subObj);
-                
-                if (leaf){
-                    addToFolder(key, obj, subObj, folder, params);
-                }
-                else{ 
-                    //is folder
-                    var subfolder = folder.addFolder(key);
-                    if (!optionsGUI.folded)
-                        subfolder.open();
+                for (var key in obj["members"]){
 
-                    params[key] = {};
-                    unfurl(obj[key], subfolder, params[key]);
+                    var subObj = obj["members"][key];
+                    if(subObj["orderIdx"] != i)
+                        continue;
+
+                    var leaf = isLeaf(subObj);
+                    
+                    if (leaf){
+                        addToFolder(key, subObj, folder, params);
+                    }
+                    else{ 
+                        //is folder
+                        var subfolder = folder.addFolder(key);
+                        if (!optionsGUI.folded)
+                            subfolder.open();
+
+                        params[key] = {};
+                        unfurl(obj["members"][key], subfolder, params[key]);
+                    }
                 }
 
             }
@@ -49,38 +71,26 @@ function guiGlue(paramsGUI, optionsGUI){
             //it is critical that none of the tracked parameters is itself an object
             function isLeaf(obj){
 
-                var Leaf = true;
-                for (var key in obj){
-
-                    if (key === 'choices' && obj.display === 'selector') continue;
-
-                    if (Leaf){
-                        var isObj = (Object.prototype.toString.call( obj[key] ) != '[object Object]');
-                        Leaf = Leaf && isObj;
-                    }
-                    else
-                        continue;
-                }
-
-                return Leaf;
+                return !obj.hasOwnProperty('members');
 
             }
 
         }
 
-        function addToFolder(key, obj, options, folder, params){
+        function addToFolder(key, options, folder, params){
 
             var handle;
             params[key] = options.value;
 
-            var display = options.display || '';
+            //type almost equals type (except float&int)
+            var type = options.type || '';
 
-            switch (display){
-                case 'range':
-                    if (options.step)
-                        handle = folder.add(params, key, options.min, options.max).step(options.step);
-                    else
-                        handle = folder.add(params, key, options.min, options.max);
+            switch (type){
+                case 'int':
+                    handle = folder.add(params, key, options.min, options.max).step(1);
+                    break;
+                case 'float':
+                    handle = folder.add(params, key, options.min, options.max);
                     break;
                 case 'selector':
                     handle = folder.add(params, key, options.choices);
@@ -102,23 +112,8 @@ function guiGlue(paramsGUI, optionsGUI){
             // pass all changes through to OF 
             if (handle){
 
-                //building up the parent path upwards
-                /*var myObj = {};
-                var pathObj = myObj;
-                var parent = folder;
-                while(parent.name !== undefined){
-                    myObj[parent.name] = {};
-                    myObj = myObj[parent.name];
-                    parent = parent.parent;
-                }
-
-                
-                var f = function(val){
-                    myObj[key] = val;
-                    var jsonString = JSON.stringify(pathObj);
-                    console.log(jsonString);
-                    socket.send(jsonString);
-                }*/
+                //listen for sync in both directions
+                handle.listen(true);
 
                 var myObj = {};
                 var path = [];
--- bin/data/web/js/main.js
+++ bin/data/web/js/main.js
@@ -2,8 +2,6 @@ var socket;
 
 var messageDiv;
 var statusDiv;
-var button;
-var textField;
 
 $(document).ready( function() {
 	
@@ -12,56 +10,28 @@ $(document).ready( function() {
 
 	messageDiv = document.getElementById("messages");
 	statusDiv = document.getElementById("status");
-
-	//setup message sending button
-	message = document.getElementById("message");
-	button = document.getElementById("button");
-
-	// send the form when you press enter 
-	// or when you press the button
-	button.onclick = function(e){
-		sendMessageForm();
-	};
-	$("#message").keyup(function(event){
-    	if(event.keyCode == 13){
-    		sendMessageForm()
-    	}
-    })
 });
-
-// send value from text input
-function sendMessageForm(){
-	/*socket.send(message.value);
-	message.value = "";*/
-}
 
 // setup web socket
 function setupSocket(){
 
 	// setup websocket
-	// get_appropriate_ws_url is a nifty function by the libwebsockets people
-	// it decides what the websocket url is based on the broswer url
-	// e.g. https://mygreathost:9099 = wss://mygreathost:9099
+	socket = new WebSocket(get_appropriate_ws_url());
 
-	if (BrowserDetect.browser == "Firefox") {
-		socket = new MozWebSocket(get_appropriate_ws_url());
-	} else {
-        //socket = new WebSocket(get_appropriate_ws_url()+":9092");
-        socket = new WebSocket("ws://localhost:9092");
-	}
-	
 	// open
 	try {
 		socket.onopen = function() {
 			statusDiv.style.backgroundColor = "#40ff40";
 			statusDiv.textContent = " websocket connection opened ";
+			var myJson = {type:"initRequest"};	
+			socket.send(JSON.stringify(myJson));
 		} 
 
 		// received message
 		socket.onmessage =function got_packet(msg) {
 			messageDiv.innerHTML = msg.data + "<br />" + messageDiv.innerHTML;
 			// console.log(msg.data);
-			initGui(msg.data);
+			śyncGui(msg.data);
 		}
 
 		socket.onclose = function(){
